aedbe0b13431ac3c054ef371196e2eeb4fd28c6b
Bryan Johnson <bjohnso5@gmail.com>
2017-01-13 19:43:57 -0500

    Update with changes ported from hue-hacking PR #4 (https://github.com/bjohnso5/hue-hacking/pull/4)

--- test/test-colors.js
+++ test/test-colors.js
@@ -15,3 +15,24 @@ exports['testCssColorAll'] = function(test) {
 	test.deepEqual(hue.setAllColors("white"), test_constants.color_white);
 	test.done();
 };
+
+var cieRGBRed = [0.6484272236872118, 0.330856101472778];
+var hexRGBGreen = '00FF00';
+var cieRGBGreen = [0.4091, 0.518];
+var hexRGBBlue = '0000FF';
+var cieRGBBlue = [0.167, 0.04];
+
+exports['known parameter returns known coordinates (full red)'] = function(test) {
+	test.deepEqual(hue.colors.CIE1931ToHex.apply(null, cieRGBRed), 'ff6c22');
+	test.done();
+};
+
+exports['known parameter returns known coordinates (full green)'] = function(test) {
+	test.deepEqual(hue.colors.CIE1931ToHex.apply(null, cieRGBGreen), 'fffe50');
+	test.done();
+};
+
+exports['known parameter returns known coordinates (full blue)'] = function(test) {
+	test.deepEqual(hue.colors.CIE1931ToHex.apply(null, cieRGBBlue), '3639ff');
+	test.done();
+};
