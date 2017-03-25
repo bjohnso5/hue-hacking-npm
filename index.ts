39fbab0e11564dda03535fe367319c2bb35ecf79
Bryan Johnson <bjohnso5@gmail.com>
2017-03-25 13:55:14 -0400

    Export HueColors class from module

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 // Export the module from the base level for ease of use
 export { Hue } from './src/hue-node';
+export { HueColors } from './src/hue-colors'; 
 export * from './src/hue-interfaces';
