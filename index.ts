a70ea9df90f6ad3eab9ad8134352b72cfc6f8992
Bryan Johnson <706204+bjohnso5@users.noreply.github.com>
2025-08-08 22:45:28 -0400

    Various tidyups (#175)
    * Update copyright notice to 2025 (... in August? meh)
    
    * Biome formatting
    
    * Bump all devtools, cleanup unused prettier config

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-// Export the module from the base level for ease of use
-export { Hue } from './src/hue-node.js';
-export { HueColors } from './src/hue-colors.js'; 
+export { HueColors } from './src/hue-colors.js';
 export * from './src/hue-interfaces.js';
+export { Hue } from './src/hue-node.js';
